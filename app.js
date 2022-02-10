98bdabb983f87c0353d0a308c1f5ca0721d54c8a
Catrina Alexandru <ovidiualex.catrina@gmail.com>
2022-02-10 02:10:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+this.addEventListener("mousemove", function(e){
+
+    let body=document.querySelector('body');
+    let bubble=document.createElement('span');
+    let a=e.offsetX;
+    let b=e.offsetY;
+    bubble.style.left='a'+'px';
+    bubble.style.top='a'+'px';
+    bubble.style.right='b'+'px';
+    bubble.style.top='a'+'px';
+ 
+    let size=Math.random()*100;
+    bubble.style.width=20 + size + "px";
+    bubble.style.height=20 + size + "px";
+    body.appendChild(bubble);
+
+
+    setTimeout(function(){
+     bubble.remove();
+    },3000);
+ 
+
+});
+
